8fd48ae00cdc3b1312ac44711c6acc0a03373e39
Deodate <ajadeo04@gmail.com>
2024-08-19 12:24:32 +0200

    Progress

--- twinview/src/App.js
+++ twinview/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navabar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello my friend!</h1>
-      <p>Greetings!</p>
+     <Navbar/>
     </div>
   );
 }
